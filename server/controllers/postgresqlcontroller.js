104c7f999ae2cd4b114a5e340d2c6d1334ce66f6
Matthew Baseman <matthew.baseman@gmail.com>
2023-05-30 14:40:18 -0700

    Got the Questions read to run at 1K+ RPS

--- server/controllers/postgresqlcontroller.js
+++ server/controllers/postgresqlcontroller.js
@@ -13,7 +13,7 @@ module.exports = {
     postgresqlModel.getQuestions(params)
       .then((result) => {
         const obj = {};
-        obj.product = params.product_id;
+        obj.product_id = params.product_id;
         obj.results = result.rows;
         res.status(200).send(obj)
       })
@@ -29,6 +29,19 @@ module.exports = {
       page: 1,
       count: 5
     }
+    postgresqlModel.getAnswers(params)
+      .then((result) => {
+        const obj = {};
+        obj.question = params.question_id;
+        obj.page = params.page;
+        obj.count = params.count;
+        obj.results = result.rows;
+        res.status(200).send(obj)
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500)
+      })
   },
 
   postQuestions: (req, res) => {
@@ -38,6 +51,14 @@ module.exports = {
       email: req.query.email,
       product_id: req.query.product_id
     }
+    postgresqlModel.postQuestions(params)
+      .then((result) => {
+        res.status(201).send('CREATED')
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500)
+      })
   },
 
   postAnswers: (req, res) => {
@@ -48,17 +69,41 @@ module.exports = {
       email: req.query.name,
       photos: req.query.photos
     }
+    postgresqlModel.postAnswers(params)
+      .then((result) => {
+        res.status(201).send('CREATED')
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500)
+      })
   },
 
   putQuestions: (req, res) => {
     const params = {
       question_id: req.query.question_id
     }
+    postgresqlModel.putQuestions(params)
+      .then((result) => {
+        res.status(204).send('NO CONTENT')
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500)
+      })
   },
 
   putAnswers: (req, res) => {
     const params = {
-      answer_id: req.query.question_id
+      answer_id: req.query.answer_id
     }
+    postgresqlModel.putAnswers(params)
+      .then((result) => {
+        res.status(204).send('NO CONTENT')
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500)
+      })
   }
 }
--- server/models/postgresqlModel.js
+++ server/models/postgresqlModel.js
@@ -1,6 +1,6 @@
 const pool = require('../db/postgresql');
 
-
+pool.connect();
 
 module.exports = {
   // connectPgDatabase: async () => {
@@ -20,11 +20,72 @@ module.exports = {
   // },
   getQuestions: (params) => {
     return pool.query(
-      `SELECT *
-      FROM questions
-      WHERE product_id = $1`,
+      `SELECT
+      q.id AS question_id,
+      q.body AS question_body,
+      q.date_written AS question_date,
+      q.asker_name,
+      q.helpful AS question_helpfulness,
+      q.reported
+      FROM questions q
+      WHERE (product_id = $1 AND q.reported = false)`,
       [params.product_id]
     );
-  }
+  },
 
+  getAnswers: (params) => {
+    return pool.query(
+      `SELECT
+      a.id AS answer_id,
+      a.body,
+      a.date_written AS date,
+      a.answerer_name,
+      a.helpful AS helpfulness
+      FROM answers a
+      WHERE (question_id = $1 AND a.reported = false)`,
+      [params.question_id]
+    );
+  },
+
+  postQuestions: (params) => {
+    return pool.query(
+      `INSERT INTO questions (
+        product_id, body, asker_name, asker_email
+      ) VALUES (
+        $1, $2, $3, $4
+      )`,
+      [params.product_id, params.body, params.name, params.email]
+    )
+  },
+
+  postAnswers: (params) => {
+    return pool.query(
+      `INSERT INTO answers (
+        question_id, body, answerer_name, answerer_email
+      ) VALUES (
+        $1, $2, $3, $4
+      )`,
+      [params.question_id, params.body, params.name, params.email]
+    )
+  },
+
+  putQuestions: (params) => {
+    return pool.query(
+      `UPDATE questions
+      SET helpful = (helpful + 1)
+      WHERE id = $1`,
+      [params.question_id]
+    )
+  },
+
+  putAnswers: (params) => {
+    return pool.query(
+      `UPDATE answers
+      SET helpful = (helpful + 1)
+      WHERE id = $1`,
+      [params.answer_id]
+    )
+  }
 };
+
+// jsonb_object_agg(a.id, jsonb_build_object('id', a.id,'body', a.body, 'date', a.date_written, 'answerer_name', a.answerer_name,'helpfulness', a.helpful)) AS answers
