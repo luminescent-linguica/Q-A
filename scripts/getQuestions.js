338c494d2d5902298e7d6fabcb34c695dbe6d4db
Matthew Baseman <matthew.baseman@gmail.com>
2023-06-11 18:54:29 -0700

    Would be great to get this on a server

--- scripts/getQuestions.js
+++ scripts/getQuestions.js
@@ -1,20 +1,20 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-export const options = {
-  stages: [
-    {duration:'1s', target: 1200},
-    {duration:'29s', target: 1200},
-  ],
-  thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(99)<2000']
-  },
-};
+// import http from 'k6/http';
+// import { sleep } from 'k6';
+// export const options = {
+//   stages: [
+//     {duration:'1s', target: 1200},
+//     {duration:'29s', target: 1200},
+//   ],
+//   thresholds: {
+//     http_req_failed: ['rate<0.01'],
+//     http_req_duration: ['p(99)<2000']
+//   },
+// };
 
-export default function() {
-  const productId = Math.floor(Math.random() * (1000010 - (1000010 - 100001)) + (1000010 - 100001))
+// export default function() {
+//   const productId = Math.floor(Math.random() * (1000010 - (1000010 - 100001)) + (1000010 - 100001))
 
-  const url = `/qa/questions?product_id=${productId}`
-  const response = http.get(url);
-  sleep(1);
-}
+//   const url = `/qa/questions?product_id=${productId}`
+//   const response = http.get(url);
+//   sleep(1);
+// }
